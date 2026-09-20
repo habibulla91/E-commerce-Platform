c13a0f596dc42925aab54ca0184917142c1b46b8
Habibulla <habibulla9177@gmail.com>
2026-09-20 22:06:50 +0600

    contact1

--- e-mart/src/pages/Contact/Contact.jsx
+++ e-mart/src/pages/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 
 import TopHeader from './../../components/header/TopHeader';
+import PrimaryButton from './../../components/UIKIT/PrimaryButton';
 function Contact() {
     return (
         <div className='w-full max-w-[--max-width]'>
@@ -9,7 +10,7 @@ function Contact() {
             <div  className='w-[90%] mx-auto'>
                 
                 <div className='grid grid-cols-4 mt-5 gap-5'>
-                    <div className='rounded p-5 shadow'>
+                    <div className='rounded p-5 border'>
                         <div>
                             <i className="bi bi-geo-alt text-[var(--salmon-pink)] font-semibold text-[length:var(--fs-1)] w-12 h-12 inline-flex items-center justify-center  rounded-full bg-[#fee8ea]"></i>
                         </div>
@@ -18,7 +19,7 @@ function Contact() {
                             <p className='text-[length:var(--fs-9)] text-[var(--sonic-silver)]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat alias dolores in architecto! </p>
                         </div>
                     </div>
-                    <div className='rounded p-5 shadow'>
+                    <div className='rounded p-5 border'>
                         <div>
                             <i className="bi bi-telephone-fill text-[var(--salmon-pink)] font-semibold text-[length:var(--fs-1)] w-12 h-12 inline-flex items-center justify-center  rounded-full bg-[#fee8ea]"></i>
                         </div>
@@ -27,7 +28,7 @@ function Contact() {
                             <p className='text-[length:var(--fs-9)] text-[var(--sonic-silver)]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat alias dolores in architecto! </p>
                         </div>
                     </div>
-                    <div className='rounded p-5 shadow'>
+                    <div className='rounded p-5 border'>
                         <div>
                             <i className="bi bi-envelope-fill text-[var(--salmon-pink)] font-semibold text-[length:var(--fs-1)] w-12 h-12 inline-flex items-center justify-center  rounded-full bg-[#fee8ea]"></i>
                         </div>
@@ -36,13 +37,14 @@ function Contact() {
                             <p className='text-[length:var(--fs-9)] text-[var(--sonic-silver)]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat alias dolores in architecto! </p>
                         </div>
                     </div>
-                    <div className='rounded p-5 shadow'>
+                    <div className='rounded p-5 border'>
                         <div>
                             <i className="bi bi-headset text-[var(--salmon-pink)] font-semibold text-[length:var(--fs-1)] w-12 h-12 inline-flex items-center justify-center  rounded-full bg-[#fee8ea]"></i>
                         </div>
                         <div>
                             <h2 className='font-bold my-1'>Live Chat</h2>
-                            <p className='text-[length:var(--fs-9)] text-[var(--sonic-silver)]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat alias dolores in architecto! </p>
+                            <p className='my-1 text-[length:var(--fs-9)] text-[var(--sonic-silver)]'>Chat with us for instant help </p>
+                            <PrimaryButton btnTxt={<i className="bi bi-chat-right-text text-white font-semibold inline-flex items-center justify-center"><span className="ml-2">Start Chat</span></i>} />
                         </div>
                     </div>
                 </div>
